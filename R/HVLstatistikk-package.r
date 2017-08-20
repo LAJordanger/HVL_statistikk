a0db0c6468e6248c7b168d4a2a643f03c6fcabd0
LAJordanger <lars.jordanger@gmail.com>
2017-08-20 10:50:25 +0200

    Initial setup for the R-package.

---
+++ R/HVLstatistikk-package.r
@@ -0,0 +1,12 @@
+#' HVLstatistikk.
+#'
+#' A package with a few simple shiny-applications, intended to be used
+#' when teaching statistics at the Western Norway Universiy of Applied
+#' Sciences.
+#'
+#' @import ggplot2
+#' @import shiny
+#' 
+#' @name HVLstatistikk
+#' @docType package
+NULL
